c0bd015fadf2646420e3f2e1a123d4ecfc5babed
harokb <harokb@gmail.com>
2016-09-28 20:38:09 +0200

    Improved algorithm, saves image and shows it

--- fractal.py
+++ fractal.py
@@ -1,11 +1,23 @@
-CONSTANT = 5+2*j
-DEPTH = 500
+import os
+
+from PIL import Image
+
+CONSTANT = 5+2*1j
+DEPTH = 1000
+
+WIDTH = 800
+HEIGHT = 600
 
 def evolution(val, constant=CONSTANT):
     return val**2 + constant
 
 def is_inside_bounds(val):
-    return (val.real ** 2 + val ** imag) < 4
+    # Fast simple evaluation
+    # return abs(val.real) < 2 and abs(val.imag) < 2
+
+    # Slower, more precise version
+    size_squared = (val.real ** 2 + val.imag ** 2)
+    return size_squared < 4
 
 def num_iterations_until_escape(constant):
     num = 0
@@ -14,10 +26,51 @@ def num_iterations_until_escape(constant):
         num = evolution(num, constant)
         iterations += 1
 
+    return iterations
+
     if iterations == DEPTH:
         return None
     else:
         return iterations
-    
 
+def get_colors(iterations):
+    if iterations == DEPTH:
+        return (255, 255, 255)
+    else:
+        return (0,0,0)
+    percent = iterations / DEPTH
+    color = 255 - int(255 * percent)
+    return (color, color, 0)
+
+def make_image(pixels_colors):
+    im = smp.toimage(pixels_colors)
+    im.save("fractal.png","PNG")
+    os.system("xdg-open fractal.png")
+    return im
+
+def scale_x(x):
+    # The Mandelbrot X scale is (-2.5, 1)
+    return -2.5 + 3.5 * (x / WIDTH)
+
+def scale_y(y):
+    # The Mandelbrot Y scale is (-1, 1)
+    return -1.0 +  2 * (y / HEIGHT)
+
+def make_mandelbrot_image():
+    colors_pixels = []
+    for x in range(WIDTH):
+        print(x)
+        scaled_x = scale_x(x)
+        for y in range(HEIGHT):
+            scaled_y = scale_y(y)
+            constant = scaled_x + scaled_y*1j
+            num_iterations = num_iterations_until_escape(constant)
+            color = get_colors(num_iterations)
+            colors_pixels.append(color)
+    print(colors_pixels)
+    with open("pixel.data", "w") as f:
+        f.write(colors_pixels)
+    make_image(colors_pixels)
+
+make_mandelbrot_image()
 
