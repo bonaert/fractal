7b8227d7d32871de2160b72ebb3efc98342dc75a
harokb <harokb@gmail.com>
2016-09-29 11:26:50 +0200

    Now puts the pixels in the right order, scale is correct, and has a nice generated palette

--- fractal.py
+++ fractal.py
@@ -1,17 +1,38 @@
 import os
+import json
 
 from PIL import Image
+DEPTH = 100
 
-CONSTANT = 5+2*1j
-DEPTH = 500
+X_SCALE = (-2.2, 2.2)
+Y_SCALE = (- 2.2, 2.2)
 
-WIDTH = 800
-HEIGHT = 600
+X_SIZE = X_SCALE[1] - X_SCALE[0]
+Y_SIZE = Y_SCALE[1] - Y_SCALE[0]
+
+DIM = 200
+WIDTH = int(DIM * X_SIZE)
+HEIGHT = int(DIM * Y_SIZE)
+
 
 ITERATIONS_FILE_NAME = "iterations.data"
 PIXEL_COLORS_FILE_NAME = "pixel.data"
 
-def evolution(val, constant=CONSTANT):
+import colorsys
+colors_max = DEPTH + 1
+# Calculate a tolerable palette
+def generate_palette():
+    palette = [0] * colors_max
+    for i in range(colors_max):
+        f = 1-abs((float(i)/colors_max-1)**15)
+        r, g, b = colorsys.hsv_to_rgb(.66+f/3, 1-f/2, f)
+        palette[i] = (int(r*255), int(g*255), int(b*255))
+    return palette
+
+
+PALETTE = generate_palette()
+
+def evolution(val, constant):
     return val**2 + constant
 
 def is_inside_bounds(val):
@@ -19,11 +40,10 @@ def is_inside_bounds(val):
     # return abs(val.real) < 2 and abs(val.imag) < 2
 
     # Slower, more precise version
-    size_squared = (val.real ** 2 + val.imag ** 2)
-    return size_squared < 4
+    return abs(val) <= 2
 
 def num_iterations_until_escape(constant):
-    num = 0
+    num = 0+0*1j
     iterations = 0
     while is_inside_bounds(num) and iterations < DEPTH:
         num = evolution(num, constant)
@@ -31,33 +51,32 @@ def num_iterations_until_escape(constant):
 
     return iterations
 
-    if iterations == DEPTH:
-        return None
-    else:
-        return iterations
-
 def get_colors(iterations):
-    if iterations == DEPTH:
-        return (255, 255, 255)
-    else:
-        return (0,0,0)
+    num = iterations
+    return PALETTE[num]
+#    if iterations == DEPTH:
+#        return (255, 255, 255)
+#    else:
+#        return (0,0,0)
     percent = iterations / DEPTH
     color = 255 - int(255 * percent)
     return (color, color, 0)
 
 def make_image(pixels_colors):
-    im = smp.toimage(pixels_colors)
+    im = Image.new("RGB", (WIDTH, HEIGHT))
+    im.putdata(pixels_colors)
     im.save("fractal.png","PNG")
     os.system("xdg-open fractal.png")
     return im
 
 def scale_x(x):
     # The Mandelbrot X scale is (-2.5, 1)
-    return -2.5 + 3.5 * (x / WIDTH)
+    return X_SCALE[0] + X_SIZE * (x / WIDTH)
 
 def scale_y(y):
     # The Mandelbrot Y scale is (-1, 1)
-    return -1.0 +  2 * (y / HEIGHT)
+    return Y_SCALE[0] + Y_SIZE * (y / HEIGHT)
+
 
 def restore_from_file(filename):
     try:
@@ -68,11 +87,11 @@ def restore_from_file(filename):
 
 def compute_iterations():
     iterations = []
-    for x in range(WIDTH):
-        print(x)
-        scaled_x = scale_x(x)
-        for y in range(HEIGHT):
-            scaled_y = scale_y(y)
+    for y in range(HEIGHT):
+        print(y)
+        scaled_y = scale_y(y)
+        for x in range(WIDTH):
+            scaled_x = scale_x(x)
             constant = scaled_x + scaled_y*1j
             num_iterations = num_iterations_until_escape(constant)
             iterations.append(num_iterations)
@@ -87,14 +106,17 @@ def get_iterations():
 
 def make_mandelbrot_image():
     iterations = get_iterations()
+    print("Got iterations")
     colors_pixels = list(map(get_colors, iterations))
-    print(colors_pixels)
+    print("Got pixel colors")
 
     with open(ITERATIONS_FILE_NAME, "w") as f:
         f.write(str(iterations))
 
     with open(PIXEL_COLORS_FILE_NAME, "w") as f:
         f.write(str(colors_pixels))
+    
+    print("Saved data")
     
     make_image(colors_pixels)
 
