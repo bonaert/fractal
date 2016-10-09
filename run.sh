a657439629cd239eb928cfe5c1ed6ae0acf2b475
harokb <harokb@gmail.com>
2016-10-09 15:58:59 +0200

    C++ compiler optimizes more: increased the optimization flag

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ ! -f ./generate ]; then
 	echo "Compiling C++ file"
-	g++-5 generate.cpp -o generate -O2
+	g++-5 generate.cpp -o generate -O3
 fi
 echo "Running Python GUI"
 python3 gui.py
